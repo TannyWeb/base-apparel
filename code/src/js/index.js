6c4e3699b8a95535848a260181c4b3b1525725d0
Tanvir Ali <Tanvir.Ali@dentsu.com>
2020-12-07 16:13:18 +0000

    first commit

---
+++ code/src/js/index.js
@@ -0,0 +1,8 @@
+// import '@fortawesome/fontawesome-free/js/all';
+import '../scss/main.scss';
+
+// import { toggleMenu } from './functions/menu';
+
+console.log('index loaded');
+// menu();
+// toggleMenu();
